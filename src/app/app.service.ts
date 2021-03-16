61999b66f1cca2c47c26231bf66083e60ca28549
unknown <pawangtm2419@gmail.com>
2021-03-16 13:27:19 +0530

    all setup

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public baseURL = "https://github.com/pawangtm2419";
+  public searchResults: any;
+
+  public searchEntry(term): Observable<any> {
+    if(term === "") {
+      console.log("input Empty.");
+    } else {
+      let parems = {q:term};
+      return this.http.get(this.baseURL, {parems}).pipe(map(res => {
+        console.log(res);
+        return this.searchResults = res["item"];
+      })
+      );
+    }
+  }
+
+  public _searchEntry(term: any) {
+    return this.searchEntry(term);
+  }
 }
